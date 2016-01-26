201d2c31372ef1e5db64711cebe1e0830d829a37
Andrea Ferretti <ferrettiandrea@gmail.com>
2016-01-26 11:09:00 +0100

    Added ScalaDoc for TransformCodec

--- src/org.velvia/msgpack/TransformCodecs.scala
+++ src/org.velvia/msgpack/TransformCodecs.scala
@@ -5,6 +5,14 @@ import java.io.DataOutputStream
 
 object TransformCodecs {
 
+  /** Defines a codec for `A` given one for `B` and a bijection `A` <=> `B`.
+    *
+    * An example would be (de)serializing a `Date` by storing its timestamp,
+    * which is just a `Long`. This would look like:
+    *
+    *     import java.util.Date
+    *     val c: Codec[Date] = new TransformCodec[Date, Long](_.getTime, new Date(_))
+    */
   class TransformCodec[A, B: Codec](
     forward: A => B,
     backward: B => A
